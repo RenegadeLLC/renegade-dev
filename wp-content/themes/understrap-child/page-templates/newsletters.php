2007fc13c017c0ca52fba8c8492d8d316ac54f04
Vince Golveo <vince.golveo@gmail.com>
2018-06-22 17:27:54 -0400

    newsletter page template with styles

--- wp-content/themes/understrap-child/page-templates/newsletters.php
+++ wp-content/themes/understrap-child/page-templates/newsletters.php
@@ -2,7 +2,8 @@
 /**
  * Template Name: Newsletters
  *
- * Template for displaying a page without sidebar even if a sidebar widget is published.
+ * Based on understrap full width  /understrap/page-templates/fullwidthpage.php
+ * Displays a page without sidebar even if a sidebar widget is published.
  *
  * @package understrap
  */
@@ -11,117 +12,32 @@ get_header();
 $container = get_theme_mod( 'understrap_container_type' );
 
 $paged = get_query_var('paged');
-
 $rn_args = array( 'post_type' => 'newsletters', 'posts_per_page' => 4, 'post_status' => 'publish', 'order' => 'DESC', 'orderby' => 'date', 'paged' => $paged  );
-
 $wp_query = new WP_Query( $rn_args );
 
 ?>
 
-<div class="wrapper" id="full-width-page-wrapper">
+<div class="wrapper main-content" id="full-width-page-wrapper">
 
-	<div class="<?php echo esc_attr( $container ); ?>" id="content">
+<div class="<?php echo esc_attr( $container ); ?>" id="newsletter-posts">
 
-		<div class="row">
+			<main class="site-main" id="main" role="main">
 
-			<div class="col-md-12 content-area" id="primary">
+					<div class="card-columns">
+						<?php while ( have_posts() ) : the_post(); ?>
+							<div class="card">
+								<div class="card-body">
+									<?php get_template_part( 'loop-templates/content', 'newsletter' ); ?>
+								</div>
+							</div>
+						<?php endwhile; // end of the loop. ?>
+					</div>
 
-				<main class="site-main" id="main" role="main">
-				<div class=" post-listing">
-<div class="grid row"  id="post-grid"><div class="grid-gutter"></div>
-					<?php while ($wp_query->have_posts() ) : $wp_query->the_post(); ?>
-
-						<?php get_template_part( 'loop-templates/content', 'newsletter' ); ?>
-
-						<?php
-						// If comments are open or we have at least one comment, load up the comment template.
-						if ( comments_open() || get_comments_number() ) :
-
-							//comments_template();
-
-						endif;
-						?>
-
-					<?php endwhile; // end of the loop. ?>
-</div><!-- .grid -->
-<div class="pagination">
-        <?php //understrap_pagination(); ?>
-        
-             <?php previous_posts_link( '« Newer posts' ); ?>
-        <?php next_posts_link( 'Older posts »', $wp_query->max_num_pages ); ?>
-        
-    </div>
-</div><!-- .post-listing -->
-				</main><!-- #main -->
-
-			</div><!-- #primary -->
-
-		</div><!-- .row end -->
-
-	</div><!-- Container end -->
+			</main>
+			
+</div>
 
 </div><!-- Wrapper end -->
 
 
-	<script type="text/javascript">
-	
-	var $container = $('#post-grid');
-
-	$(window).on('load', function () {
-	    // Fire Isotope only when images are loaded
-	    $container.imagesLoaded(function () {
-	        $container.isotope({
-	            itemSelector: '.grid-item', 
-	       	 	percentPosition: true,
-	       		isOriginTop: true,
-	   	  		layoutMode: 'packery',
-	            	packery: {
-	          	  // masonry: {
-	      	      gutter: '.grid-gutter'
-	      	      }
-	           
-	        });
-	    });
-
-	
-
-	    // Infinite Scroll
-	    $('#post-grid').infinitescroll({
-	            navSelector: 'div.pagination',
-	            nextSelector: 'div.pagination a:first',
-	            itemSelector: '.grid-item',
-	            bufferPx: 200,
-	            loading: {
-	                finishedMsg: 'We\'re done here.',
-	                //img: +templateUrl+'ajax-loader.gif'
-	            },
-	        },
-
-	        // Infinite Scroll Callback
-	        function (newElements) {
-	            var $newElems = jQuery(newElements).hide();
-	            $newElems.imagesLoaded(function () {
-	                $newElems.fadeIn();
-	                $container.isotope('appended', $newElems);
-	            });
-	        });
-	});
-	$( function() {
-	  
-	  $('.grid4').isotope({
-		 percentPosition: true,
-	  layoutMode: 'packery',
-	  //   layoutMode: 'masonry',
-	    itemSelector: '.grid-item',
-	    isOriginTop: true,
-	    packery: {
-	  // masonry: {
-	      gutter: '.grid-gutter4'
-	      }
-	  });
-	  
-	});
-
-
-	</script>
 <?php get_footer(); ?>
