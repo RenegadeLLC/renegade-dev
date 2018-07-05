0491d0afd64d50dbb4d10a494ea680df2784a319
Vince Golveo <vince.golveo@gmail.com>
2018-07-05 15:30:54 -0400

    refactor load more scripts

--- wp-content/themes/understrap-child/functions.php
+++ wp-content/themes/understrap-child/functions.php
@@ -38,7 +38,6 @@ define('FUNCTIONS', get_stylesheet_directory() . '/library/functions');
 require_once( get_stylesheet_directory() . '/library/javascript_loader.php' );
 // require_once( get_stylesheet_directory() . '/library/css_loader.php' );
 require_once( get_stylesheet_directory() . '/library/functions_loader.php' );
-
 // require_once( get_stylesheet_directory() . '/includes/theme-styles.php' );
 
 
@@ -66,66 +65,14 @@ function change_date( $post_id ) {
 }
 
 // enqueue_scripts: make sure to include ajaxurl, so we know where to send the post request
-function dt_add_main_js(){
+// function dt_add_main_js(){
     
-    wp_register_script( 'main-js', get_stylesheet_directory_uri() . '/library/js/load-more-script.js', array( 'jquery' ), '1.0', false );
-    wp_enqueue_script( 'main-js' );
-    wp_localize_script( 'main-js', 'headJS', array( 'ajaxurl' => admin_url( 'admin-ajax.php' ), 'templateurl' => get_stylesheet_directory(), 'posts_per_page' => get_option('posts_per_page') ) );
+//     wp_register_script( 'main-js', get_stylesheet_directory_uri() . '/library/js/load-more-script.js', array( 'jquery' ), '1.0', false );
+//     wp_enqueue_script( 'main-js' );
+//     wp_localize_script( 'main-js', 'headJS', array( 'ajaxurl' => admin_url( 'admin-ajax.php' ), 'templateurl' => get_stylesheet_directory(), 'posts_per_page' => get_option('posts_per_page') ) );
     
-}
-add_action( 'wp_enqueue_scripts', 'dt_add_main_js', 90);
-
-
-add_action( "wp_ajax_load_more", "load_more_func" ); // when logged in
-add_action( "wp_ajax_nopriv_load_more", "load_more_func" );//when logged out
-//function return new posts based on offset and posts per page value
-function load_more_func() {
-    //verifying nonce here
-    if ( !wp_verify_nonce( $_REQUEST['nonce'], "load_posts" ) ) {
-        exit("No naughty business please");
-    }
-    $offset = isset($_REQUEST['offset'])?intval($_REQUEST['offset']):0;
-    $posts_per_page = isset($_REQUEST['posts_per_page'])?intval($_REQUEST['posts_per_page']):10;
-    //optional, if post type is not defined use regular post type
-    $post_type = isset($_REQUEST['post_type'])?$_REQUEST['post_type']:'post';
-    
-    
-    ob_start(); // buffer output instead of echoing it
-    $args = array(
-        'post_type'=>$post_type,
-        'offset' => $offset,
-        'posts_per_page' => $posts_per_page,
-        'orderby' => 'date',
-        'order' => 'DESC'
-    );
-    $posts_query = new WP_Query( $args );
-    
-    
-    if ($posts_query->have_posts()) {
-        //if we have posts:
-        $result['have_posts'] = true; //set result array item "have_posts" to true
-        
-        while ( $posts_query->have_posts() ) : $posts_query->the_post(); ?>
-			<article id="post-<?php the_ID(); ?>" class="single-article" >
-				<?php //here goes your post content:?>
-				<h1><?php the_title(); ?></h1>
-				<?php the_content(); ?>
-			</article>
-			<?php endwhile;
-		$result['html'] = ob_get_clean(); // put alloutput data into "html" item
-  } else {
-	  //no posts found
-	  $result['have_posts'] = false; // return that there is no posts found
-  } 
-        if(!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
-            $result = json_encode($result); // encode result array into json feed
-            echo $result; // by echo we return JSON feed on POST request sent via AJAX
-        }
-        else { 
-            header("Location: ".$_SERVER["HTTP_REFERER"]);
-        }
-  die();
-}
+// }
+// add_action( 'wp_enqueue_scripts', 'dt_add_main_js', 90);
 
 /***** ADD NEW MENUS TO THEME ********/
 
@@ -178,3 +125,46 @@ if( function_exists('acf_add_options_page') ) {
     ));
     
 }
+
+/**
+ * Javascript for Load More
+ * 
+ */
+
+function rn_load_more_js() {
+	global $wp_query;
+	$args = array(
+		'nonce' => wp_create_nonce( 'rn-load-more-nonce' ),
+		'url'   => admin_url( 'admin-ajax.php' ),
+		'query' => $wp_query->query,
+	);
+    
+    
+    if ( get_post_type() === 'post' ) {
+	    wp_enqueue_script( 'rn-load-more', get_stylesheet_directory_uri() . '/library/js/load-more.js', array( 'jquery' ), '1.0', true );
+        wp_localize_script( 'rn-load-more', 'rnloadmore', $args );
+    }
+	
+}
+add_action( 'wp_enqueue_scripts', 'rn_load_more_js' );
+/**
+ * AJAX Load More 
+ *
+ */
+function rn_ajax_load_more() {
+	check_ajax_referer( 'rn-load-more-nonce', 'nonce' );
+    
+	$args = isset( $_POST['query'] ) ? $_POST['query'] : array();
+	$args['post_type'] = isset( $args['post_type'] ) ? $args['post_type'] : 'post';
+	$args['paged'] = $_POST['page'];
+	$args['post_status'] = 'publish';
+	ob_start();
+	$loop = new WP_Query( $args );
+	if( $loop->have_posts() ): while( $loop->have_posts() ): $loop->the_post();
+		rn_post_summary();
+	endwhile; endif; wp_reset_postdata();
+	$data = ob_get_clean();
+	wp_send_json_success( $data );
+}
+add_action( 'wp_ajax_rn_ajax_load_more', 'rn_ajax_load_more' );
+add_action( 'wp_ajax_nopriv_rn_ajax_load_more', 'rn_ajax_load_more' );
