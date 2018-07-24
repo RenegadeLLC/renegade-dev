5ad2a58ed6e695333f4da26a38d8e2da970c4583
Vince Golveo <vince.golveo@gmail.com>
2018-07-24 13:58:07 -0400

    add subscribe form to about page

--- wp-content/themes/understrap-child/page-templates/about.php
+++ wp-content/themes/understrap-child/page-templates/about.php
@@ -6,13 +6,13 @@
  */
 
 get_header();
-$container = get_theme_mod( 'understrap_container_type' );
+$container = get_theme_mod('understrap_container_type');
 
 ?>
 
 <div class="wrapper" id="full-width-page-wrapper">
 
-	<div class="<?php echo esc_attr( $container ); ?>" id="content">
+	<div class="<?php echo esc_attr($container); ?>" id="content">
 
 		<div class="row jumbotron jumbotron-fluid">
 			<!-- Placehoder -->
@@ -29,66 +29,79 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 				<div class="row">
 					<div class="col-md-12">
-						<?php while ( have_posts() ) : the_post(); ?>
-						<?php the_content(); ?> <!-- Page Content -->
-						<?php endwhile; // end of the loop. ?>
+						<?php while (have_posts()): the_post();?>
+							<?php the_content();?> <!-- Page Content -->
+							<?php endwhile; // end of the loop. ?>
 					</div>
 				</div>
 
 				<!-- secondary content grids -->
 				<!-- CASE STUDIES -->
 				<?php
-					$ra_args = array('post_type' => 'projects', 'posts_per_page' => 3,  'post_status' => 'publish', 'order' => 'DESC', 'orderby' => 'date');
-					$wp_query = new WP_Query($ra_args); 
-				?>
+$ra_args = array('post_type' => 'projects', 'posts_per_page' => 3, 'post_status' => 'publish', 'order' => 'DESC', 'orderby' => 'date');
+$wp_query = new WP_Query($ra_args);
+?>
 				<div class="row">
 					<div class="col-md-12"><h2>Case Studies</h2></div>
 					<?php while (have_posts()): the_post();?>
-					<div class="col-lg-4 col-md-6">
-						<?php $post_type = get_post_type();
-							if ($post_type == 'projects'):
-								get_template_part('loop-templates/content', 'project');
-							endif;
-						?>
-					</div>
-					<?php endwhile; // end of the loop. ?>
+						<div class="col-lg-4 col-md-6">
+							<?php $post_type = get_post_type();
+    if ($post_type == 'projects'):
+        get_template_part('loop-templates/content', 'project');
+    endif;
+    ?>
+						</div>
+						<?php endwhile; // end of the loop. ?>
 				</div>
 
 				<!-- CLIENTS -->
 				<?php
-					// make sure to exclude renegade client post by ID number: 6274
-					$ra_args = array('post_type' => 'clients', 'posts_per_page' => 16, 'post__not_in' => array(6274),  'post_status' => 'publish', 'order' => 'DESC', 'orderby' => 'date');
-					$wp_query = new WP_Query($ra_args); 
-				?>
+// make sure to exclude renegade client post by ID number: 6274
+$ra_args = array('post_type' => 'clients', 'posts_per_page' => 16, 'post__not_in' => array(6274), 'post_status' => 'publish', 'order' => 'DESC', 'orderby' => 'date');
+$wp_query = new WP_Query($ra_args);
+?>
 				<div class="row">
 					<div class="col-md-12"><h2>Clients</h2></div>
-					<?php while (have_posts()): the_post(); ?>
-						<div class="col-lg-3 col-md-4">
-						<?php $post_type = get_post_type();
-							if ($post_type == 'clients'):
-								get_template_part('loop-templates/content', 'client');
-							endif;
-						?>
-						</div>
-					<?php endwhile; // end of the loop. ?>
+					<?php while (have_posts()): the_post();?>
+							<div class="col-lg-3 col-md-4">
+							<?php $post_type = get_post_type();
+    if ($post_type == 'clients'):
+        get_template_part('loop-templates/content', 'client');
+    endif;
+    ?>
+							</div>
+						<?php endwhile; // end of the loop. ?>
 				</div>
 
 				<!-- LEADERSHIP -->
 				<?php
-					$ra_args = array('post_type' => 'people', 'posts_per_page' => 6,  'post_status' => 'publish', 'order' => 'ASC', 'orderby' => 'date');
-					$wp_query = new WP_Query($ra_args); 
-				?>
+$ra_args = array('post_type' => 'people', 'posts_per_page' => 6, 'post_status' => 'publish', 'order' => 'ASC', 'orderby' => 'date');
+$wp_query = new WP_Query($ra_args);
+?>
 				<div class="row">
 					<div class="col-md-12"><h2>Leadership Team</h2></div>
 					<?php while (have_posts()): the_post();?>
-					<div class="col-lg-4 col-md-6">
-						<?php $post_type = get_post_type();
-							if ($post_type == 'people'):
-								get_template_part('loop-templates/content', 'people');
-							endif;
-						?>
+						<div class="col-lg-4 col-md-6">
+							<?php $post_type = get_post_type();
+    if ($post_type == 'people'):
+        get_template_part('loop-templates/content', 'people');
+    endif;
+    ?>
+						</div>
+						<?php endwhile; // end of the loop. ?>
+				</div>
+
+				<!-- SUBSCRIBE FORM -->
+				
+				<div class="row">
+					<div class="col-lg-6 col-sm-12">
+						<h1 style="color:#000000;">Subscribe</h1>
+						<h3 style="color:#000000;">to the Latest in</h3>
+						<h3 style="color:;"> Renegade Thinking</h3>
 					</div>
-					<?php endwhile; // end of the loop. ?>
+					<div class="col-lg-6 col-sm-12">
+						<?php echo do_shortcode('[mc4wp_form id="5405"]') ?>
+					</div>';
 				</div>
 
                 </main><!-- #main -->
@@ -101,4 +114,4 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 </div><!-- Wrapper end -->
 
-<?php get_footer(); ?>
+<?php get_footer();?>
