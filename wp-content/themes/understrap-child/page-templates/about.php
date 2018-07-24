e3f1d8d6c7431f8ef8c01b97e144e1d52f7ba760
Vince Golveo <vince.golveo@gmail.com>
2018-07-24 14:33:24 -0400

    fix to about page headline

--- wp-content/themes/understrap-child/page-templates/about.php
+++ wp-content/themes/understrap-child/page-templates/about.php
@@ -16,8 +16,10 @@ $container = get_theme_mod('understrap_container_type');
 
 		<div class="row jumbotron jumbotron-fluid">
 			<!-- Placehoder -->
-			<div class="col-md-12 hero">
+			<div class="col-md-8">
 				<h2>At Renegade we help courageous marketers cut through their content nightmares.</h2>
+			</div>
+			<div class="col-md-4">
 			</div>
 		</div>
 
@@ -92,12 +94,11 @@ $wp_query = new WP_Query($ra_args);
 				</div>
 
 				<!-- SUBSCRIBE FORM -->
-				
 				<div class="row">
 					<div class="col-lg-6 col-sm-12">
-						<h1 style="color:#000000;">Subscribe</h1>
-						<h3 style="color:#000000;">to the Latest in</h3>
-						<h3 style="color:;"> Renegade Thinking</h3>
+						<h1>Subscribe</h1>
+						<h3>to the Latest in</h3>
+						<h3> Renegade Thinking</h3>
 					</div>
 					<div class="col-lg-6 col-sm-12">
 						<?php echo do_shortcode('[mc4wp_form id="5405"]') ?>
