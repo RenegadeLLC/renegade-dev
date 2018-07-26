e31ed6ffd58f27b785556c8d8bd44c4f4492a62e
Vince Golveo <vince.golveo@gmail.com>
2018-07-26 14:58:26 -0400

    fix acf landing page template block

--- wp-content/themes/understrap-child/dist/page-templates/landing_page.php
+++ wp-content/themes/understrap-child/dist/page-templates/landing_page.php
@@ -11,168 +11,162 @@ global $columnHTML;
 global $headlineHTML;
 
 get_header();
-$container = get_field('container_width', 'option');
+$container = get_theme_mod('understrap_container_type');
 
 $pageHTML = '';
 
 ?>
 
-<div class="wrapper main-content" id="full-width-page-wrapper">
+<div class="wrapper" id="full-width-page-wrapper">
 
-<?php
+	<div class="<?php echo esc_attr($container); ?>" id="content">
+        
+        <div class="row">
 
-if ($container == 'Fixed Width Container'):
-    $pageHTML = '<div class="container" id="content">';
-elseif ($container == 'Full Width Container'):
-    $pageHTML = '<div class="container" id="content">';
-endif; //END CONTAINER WIDTH IF
-?>
-<div class="row">
-
-			<div class="col-md-12 content-area" id="primary">
+			<div class="content-area" id="primary">
 
 				<main class="site-main" id="main" role="main">
 
-<?php
+                    <?php
 
-//CHECK FOR CONTENT SECTIONS
-if (have_rows('content_sections')):
+                    //CHECK FOR CONTENT SECTIONS
+                    if (have_rows('content_sections')):
 
-    while (have_rows('content_sections')): the_row();
+                        while (have_rows('content_sections')): the_row();
 
-        //GET SECTION NAME
-        $content_section_name = get_sub_field('content_section_name');
-        //MAKE SECTION NAME LINK FRIENDLY
-        $content_section_name = clean_link_name($content_section_name);
+                            //GET SECTION NAME
+                            $content_section_name = get_sub_field('content_section_name');
+                            //MAKE SECTION NAME LINK FRIENDLY
+                            $content_section_name = clean_link_name($content_section_name);
 
-        //GET SECTION TYPE
-        $content_section_type = get_sub_field('content_section_type');
+                            //GET SECTION TYPE
+                            $content_section_type = get_sub_field('content_section_type');
 
-        //CUSTOMIZE SECTION BACKGROUND
-        $customize_section_background = get_sub_field('customize_section_background');
+                            //CUSTOMIZE SECTION BACKGROUND
+                            $customize_section_background = get_sub_field('customize_section_background');
 
-        if ($customize_section_background == 'Yes'):
-            $section_background_options = get_sub_field('section_background_options');
-            $background_color = $section_background_options['background_color'];
-            $background_image = $section_background_options['background_image'];
-            $customize_background_image = $section_background_options['customize_background_image'];
+                            if ($customize_section_background == 'Yes'):
+                                $section_background_options = get_sub_field('section_background_options');
+                                $background_color = $section_background_options['background_color'];
+                                $background_image = $section_background_options['background_image'];
+                                $customize_background_image = $section_background_options['customize_background_image'];
 
-            //CUSTOMIZE BACKGROUND IMAGE
-            if ($customize_background_image == 'Yes'):
-                $background_image_repeat = $section_background_options['background_image_repeat'];
-                $background_image_position = $section_background_options['background_image_position'];
-                $background_image_size = $section_background_options['background_image_size'];
+                                //CUSTOMIZE BACKGROUND IMAGE
+                                if ($customize_background_image == 'Yes'):
+                                    $background_image_repeat = $section_background_options['background_image_repeat'];
+                                    $background_image_position = $section_background_options['background_image_position'];
+                                    $background_image_size = $section_background_options['background_image_size'];
 
-            else:
+                                else:
 
-                $background_image_repeat = 'no-repeat';
-                $background_image_position = 'center';
-                $background_image_size = 'cover';
+                                    $background_image_repeat = 'no-repeat';
+                                    $background_image_position = 'center';
+                                    $background_image_size = 'cover';
 
-            endif; //END IF CUSTOMIZE BACKGROUND IMAGE
+                                endif; //END IF CUSTOMIZE BACKGROUND IMAGE
 
-        endif; //END IF CUSTOMIZE SECTION BACKGROUND
+                            endif; //END IF CUSTOMIZE SECTION BACKGROUND
 
-        //ADD CONTENT SECTION DIV
-        $pageHTML .= '<div class="content-section"';
-        //ADD A SECTION HEADLINE
+                            //ADD CONTENT SECTION DIV
+                            $pageHTML .= '<div class="content-section"';
+                            //ADD A SECTION HEADLINE
 
-        if ($customize_section_background == 'Yes'):
+                            if ($customize_section_background == 'Yes'):
 
-            $pageHTML .= ' style="';
+                                $pageHTML .= ' style="';
 
-            //CUSTOM SECTION BACKGROUND STYLING
+                                //CUSTOM SECTION BACKGROUND STYLING
 
-            if ($background_color):
-                $pageHTML .= 'background-color:' . $background_color . '; ';
-            endif;
+                                if ($background_color):
+                                    $pageHTML .= 'background-color:' . $background_color . '; ';
+                                endif;
 
-            if ($background_image):
-                $pageHTML .= 'background-image:url(' . $background_image . '); ';
+                                if ($background_image):
+                                    $pageHTML .= 'background-image:url(' . $background_image . '); ';
 
-                if ($background_image_repeat):
-                    $pageHTML .= ' background-repeat:' . $background_image_repeat . ';';
-                else:
-                    $pageHTML .= ' background-repeat:no-repeat;';
-                endif;
+                                    if ($background_image_repeat):
+                                        $pageHTML .= ' background-repeat:' . $background_image_repeat . ';';
+                                    else:
+                                        $pageHTML .= ' background-repeat:no-repeat;';
+                                    endif;
 
-                if ($background_image_position):
-                    $pageHTML .= 'background-position:' . $background_image_position . ';';
-                endif;
+                                    if ($background_image_position):
+                                        $pageHTML .= 'background-position:' . $background_image_position . ';';
+                                    endif;
 
-                if ($background_image_size):
-                    $pageHTML .= ' background-size:' . $background_image_size . ';';
-                endif;
+                                    if ($background_image_size):
+                                        $pageHTML .= ' background-size:' . $background_image_size . ';';
+                                    endif;
 
-            endif;
+                                endif;
 
-            $pageHTML .= '" id="' . $content_section_name . '"';
+                                $pageHTML .= '" id="' . $content_section_name . '"';
 
-        endif; //end if customize section background
+                            endif; //end if customize section background
 
-        $pageHTML .= '>'; //end of div declaration
+                            $pageHTML .= '>'; //end of div declaration
 
-        $add_a_section_headline = get_sub_field('add_a_section_headline');
+                            $add_a_section_headline = get_sub_field('add_a_section_headline');
 
-        if ($add_a_section_headline == 'Yes'):
+                            if ($add_a_section_headline == 'Yes'):
 
-            require FUNCTIONS . '/section_headline.php';
+                                require FUNCTIONS . '/section_headline.php';
 
-        endif; //END IF ADD SECTION HEADLINE
+                            endif; //END IF ADD SECTION HEADLINE
 
-        if ($content_section_type == 'Custom Section'):
+                            if ($content_section_type == 'Custom Section'):
 
-            $pageHTML .= $headlineHTML;
-            require FUNCTIONS . '/custom_content.php';
+                                $pageHTML .= $headlineHTML;
+                                require FUNCTIONS . '/custom_content.php';
 
-        elseif ($content_section_type == 'Client Logo Grid'):
+                            elseif ($content_section_type == 'Client Logo Grid'):
 
-            $pageHTML .= $headlineHTML;
-            $clientsHTML = make_logo_grid();
-            $pageHTML .= $clientsHTML;
+                                $pageHTML .= $headlineHTML;
+                                $clientsHTML = make_logo_grid();
+                                $pageHTML .= $clientsHTML;
 
-        elseif ($content_section_type == 'Team Bio'):
+                            elseif ($content_section_type == 'Team Bio'):
 
-            $team_member = get_sub_field('team_bio');
-            $bioHTML = do_bio($team_member, $headlineHTML);
-            $pageHTML .= $bioHTML;
+                                $team_member = get_sub_field('team_bio');
+                                $bioHTML = do_bio($team_member, $headlineHTML);
+                                $pageHTML .= $bioHTML;
 
-        elseif ($content_section_type == 'Post Feed'):
+                            elseif ($content_section_type == 'Post Feed'):
 
-            $pageHTML .= $headlineHTML;
-            $feedHTML = build_feed();
-            $pageHTML .= $feedHTML;
+                                $pageHTML .= $headlineHTML;
+                                $feedHTML = build_feed();
+                                $pageHTML .= $feedHTML;
 
-        elseif ($content_section_type == 'Subscribe Form'):
+                            elseif ($content_section_type == 'Subscribe Form'):
 
-            $subscribe_form_shortcode = get_sub_field('subscribe_form_shortcode');
-            $formHTML = build_subscribe_form($subscribe_form_shortcode, $headlineHTML);
-            $pageHTML .= $formHTML;
+                                $subscribe_form_shortcode = get_sub_field('subscribe_form_shortcode');
+                                $formHTML = build_subscribe_form($subscribe_form_shortcode, $headlineHTML);
+                                $pageHTML .= $formHTML;
 
-        elseif ($content_section_type == 'Testimonials'):
+                            elseif ($content_section_type == 'Testimonials'):
 
-            $pageHTML .= $headlineHTML;
-            $testimonialsHTML = build_testimonials();
-            $pageHTML .= $testimonialsHTML;
+                                $pageHTML .= $headlineHTML;
+                                $testimonialsHTML = build_testimonials();
+                                $pageHTML .= $testimonialsHTML;
 
-        elseif ($content_section_type == 'Case Studies'):
+                            elseif ($content_section_type == 'Case Studies'):
 
-            $pageHTML .= $headlineHTML;
-            $casesHTML = build_case_studies();
-            $pageHTML .= $casesHTML;
+                                $pageHTML .= $headlineHTML;
+                                $casesHTML = build_case_studies();
+                                $pageHTML .= $casesHTML;
 
-        endif; // END CONTENT SECTION TYPE IF
+                            endif; // END CONTENT SECTION TYPE IF
 
-        //END CONTENT SECTION DIV
-        $pageHTML .= '</div><!-- .content-section #' . $content_section_name . '-->';
+                            //END CONTENT SECTION DIV
+                            $pageHTML .= '</div><!-- .content-section #' . $content_section_name . '-->';
 
-    endwhile; //END CONTENT SECTIONS WHILE
+                        endwhile; //END CONTENT SECTIONS WHILE
 
-endif; // END CONTENT SECTIONS IF
+                    endif; // END CONTENT SECTIONS IF
 
-echo $pageHTML;
+                    echo $pageHTML;
 
-?>
+                    ?>
 
 
 				</main><!-- #main -->
