a05f28f0794d76ed43d9c207d4a81a6b98e839f5
Vince Golveo <vince.golveo@gmail.com>
2018-07-31 13:28:04 -0400

    add code for infinite scrolling within custom fields

--- wp-content/themes/understrap-child/library/functions/post_feed.php
+++ wp-content/themes/understrap-child/library/functions/post_feed.php
@@ -5,7 +5,7 @@
 function build_feed(){  
     
     $feedHTML = '';
-    $feedHTML .= '<div class="grid row">';
+    // $feedHTML .= '<div class="grid row">';
     
     ob_start();
     
@@ -23,7 +23,8 @@ function build_feed(){
         $feed_type = get_sub_field('feed_type');
         
         if($feed_type == 'Pinned Posts'):
-        
+            $feedHTML .= '<div class="row">';
+            $feedHTML .= '<div class="card-columns">';
             if(have_rows('pinned_posts')):
                 while ( have_rows('pinned_posts') ) : the_row();
 
@@ -32,7 +33,7 @@ function build_feed(){
                     setup_postdata( $post ); 
                     
                     $post_type = get_post_type($post -> ID);
-                    echo '<div class="col-md-4 col-sm-6 grid__item">';	
+                    // echo '<div class="col-md-4 col-sm-6 card">';	
                     if($post_type == 'podcasts'):
                         //get_template_part( '/loop-templates/content', 'podcast' );
                         include($loop_templates.'content-podcast.php');
@@ -46,10 +47,11 @@ function build_feed(){
                         //get_template_part( '/loop-templates/content', 'video' );
                         include($loop_templates.'content-video.php');
                     endif;
-                    echo '</div>';	
+                    // echo '</div>';	
                     wp_reset_postdata();
                 endwhile;
-               
+                echo '</div>';	
+                echo '</div><!-- .row -->';
             endif;
             
             
@@ -60,6 +62,10 @@ function build_feed(){
             $number_of_posts_to_include = get_sub_field('number_of_posts_to_include');
             $included_post_types = get_sub_field('included_post_types');
             $post_type_array = [];
+
+            // $feedHTML .= '<div class="grid row">';
+            $feedHTML .= '<div class="grid row">';
+            // $feedHTML .= '<div class="card-columns">';
             
             if(!$number_of_posts_to_include):
             $number_of_posts_to_include = -1;
@@ -68,9 +74,17 @@ function build_feed(){
             foreach($included_post_types as $post_type):
             array_push($post_type_array, $post_type);
             endforeach;
+
+            $paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
             
-            
-            $rpd_args = array( 'post_type' => $post_type_array, 'posts_per_page' => $number_of_posts_to_include, 'post_status' => 'publish', 'order' => 'DESC', 'orderby' => 'date', 'paged' => $paged  );
+            $rpd_args = array( 
+                'post_type' => $post_type_array, 
+                'posts_per_page' => $number_of_posts_to_include, 
+                'post_status' => 'publish', 
+                'order' => 'DESC', 
+                'orderby' => 'date', 
+                'paged' => $paged  
+            );
             // $rpd_args = array( 'post_type' => array('newsletters', 'articles', 'podcasts'), 'posts_per_page' => $number_of_posts_to_include, 'post_status' => 'publish', 'order' => 'DESC', 'orderby' => 'date', 'paged' => $paged  );
             
             $wp_query = new WP_Query( $rpd_args );
@@ -101,10 +115,38 @@ function build_feed(){
             if ( comments_open() || get_comments_number() ) :
             
             //comments_template();
+
+            
             
             endif;
             endwhile; // end of the loop.
+
+            // echo '</div><!-- card-columns -->';
         
+            // clean up after the query and pagination
+            wp_reset_postdata(); 
+            // echo '</div>';
+            echo '</div><!-- .row -->';
+
+            echo '<!-- scroller button -->';
+            echo '<div class="row">';
+            echo '<div class="col-lg-3"></div>';
+            echo '<div class="col-lg-6">';
+            echo '<div><button type="button" class="btn btn-primary btn-block view-more-button">VIEW MORE</button></div>';
+            echo '<div class="loader-wheel .infinite-scroll-request">';
+            echo '<i><i><i><i><i><i><i><i><i><i><i><i>';
+            echo '</i></i></i></i></i></i></i></i></i></i></i></i>';
+            echo '</div>';
+            echo '</div>';
+            echo '<div class="col-lg-3"></div>';
+            echo '</div>';
+
+
+            // next_posts_link() usage with max_num_pages
+            next_posts_link( 'Older Entries', $wp_query->max_num_pages );
+            previous_posts_link( 'Newer Entries' );
+            understrap_pagination();
+
         endif;
         
         
@@ -116,7 +158,6 @@ function build_feed(){
     endif;
     ob_end_clean();
     // $feedHTML .= '</div><!-- .card-columns -->';
-    $feedHTML .= '</div><!-- .row -->';
     return $feedHTML;
 }
 
