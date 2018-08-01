67f68e42b180e556ecfcfc8424b93012fc07877c
Vince Golveo <vince.golveo@gmail.com>
2018-08-01 15:58:43 -0400

    partial work on custom post feed aggregation

--- wp-content/themes/understrap-child/library/functions/post_feed.php
+++ wp-content/themes/understrap-child/library/functions/post_feed.php
@@ -9,16 +9,14 @@ function build_feed(){
     
     $theme = get_stylesheet_directory();
     $loop_templates = $theme . '/loop-templates/';
-    
    
     $paged = get_query_var('paged');
 
-   
+   // PINNED POSTS
     if(have_rows('post_feed_items')):
     
     while ( have_rows('post_feed_items') ) : the_row();
 
-   
         $feed_type = get_sub_field('feed_type');
         
         if($feed_type == 'Pinned Posts'):
@@ -32,21 +30,15 @@ function build_feed(){
                     setup_postdata( $post ); 
                     
                     $post_type = get_post_type($post -> ID);
-                    // echo '<div class="col-md-4 col-sm-6 card">';	
                     if($post_type == 'podcasts'):
-                        //get_template_part( '/loop-templates/content', 'podcast' );
                         include($loop_templates.'content-podcast.php');
                     elseif($post_type == 'newsletters'):
-                        //get_template_part( '/loop-templates/content', 'newsletter' );
                         include($loop_templates.'content-newsletter.php');
                     elseif($post_type == 'post'):
-                        //get_template_part( '/loop-templates/content', 'post' );
                         include($loop_templates.'content-post.php');
                     elseif($post_type == 'videos'):
-                        //get_template_part( '/loop-templates/content', 'video' );
                         include($loop_templates.'content-video.php');
                     endif;
-                    // echo '</div>';	
                     wp_reset_postdata();
                 endwhile;
                 echo '</div>';	
@@ -56,56 +48,151 @@ function build_feed(){
             
         endif;
         
+        // DYNAMIC POSTS
         if($feed_type == 'Dynamic Post Feed' || $feed_type == 'Pinned Posts and Dynamic Post Feed'):
-        
+            // acf data
             $number_of_posts_to_include = get_sub_field('number_of_posts_to_include');
             $included_post_types = get_sub_field('included_post_types');
             $post_type_array = [];
-            $feedHTML .= '<div class="grid row">';
-            
             if(!$number_of_posts_to_include):
-            $number_of_posts_to_include = -1;
+                $number_of_posts_to_include = -1;
             endif;
-            
             foreach($included_post_types as $post_type):
-            array_push($post_type_array, $post_type);
+                array_push($post_type_array, $post_type);
             endforeach;
 
-            $paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
+            // get the pinned 
 
-            // GRID
+            // top posts
+            $top_posts = [];
+
+            // pinned posts
+            $pinned_posts = [];
+            if(have_rows('pinned_posts')):
+                echo '<div class="row">';
+                echo '<div class="col-md-12">';
+                echo '<h2>PINNED POSTS</h2>';
+                echo '<ul>';
+                while ( have_rows('pinned_posts') ) : the_row();
+                    $pinned_post = get_sub_field('pinned_post');
+                    $pinned_post_ID = $pinned_post -> ID;
+                    array_push($top_posts, $pinned_post_ID);
+                    // $post = $pinned_post;
+                    setup_postdata( $pinned_post ); 
+                    echo '<li><a href="' . get_permalink($pinned_post -> ID) . '">' .  get_post_type($pinned_post -> ID) . ' ' . $pinned_post_ID . ': ' . get_the_title( $pinned_post -> ID ) .'</a> </li> ';
+                endwhile;
+                echo '</ul>';
+            endif;
+            wp_reset_query();
+            print_r($top_posts);
+            echo '</div>';
+            echo '</div>';
+            
+
+            // aggregate most recent stories in feed
+            echo '<div class="row">';
+            echo '<div class="col-md-12">';
+            echo '<h2>RECENT POSTS</h2>';
+
+            $num_show = 4;
+            $recent_posts = wp_get_recent_posts(array(
+                'posts_per_page'   => $num_show,
+                'orderby'          => 'date',
+                'order'            => 'DESC',
+                'post_type'        => 'post',
+                'post_status'      => 'publish',
+            ));
+            $recent_news = wp_get_recent_posts(array(
+                'posts_per_page'   => $num_show,
+                'orderby'          => 'date',
+                'order'            => 'DESC',
+                'post_type'        => 'newsletters',
+                'post_status'      => 'publish',
+            ));
+            $recent_podcasts = wp_get_recent_posts(array(
+                'posts_per_page'   => $num_show,
+                'orderby'          => 'date',
+                'order'            => 'DESC',
+                'post_type'        => 'podcasts',
+                'post_status'      => 'publish',
+            ));
+            $recent_downloads = wp_get_recent_posts(array(
+                'posts_per_page'   => $num_show,
+                'orderby'          => 'date',
+                'order'            => 'DESC',
+                'post_type'        => 'downloads',
+                'post_status'      => 'publish',
+            ));
+            $recent_all = [];
+            for ($x = 0; $x <= $num_show; $x++) {
+                // echo "The number is: $x <br>";
+                array_push(
+                    $recent_all,
+                    $recent_posts[$x],
+                    $recent_news[$x],
+                    $recent_podcasts[$x],
+                    $recent_downloads[$x]
+                );
+            }
+            $recent_all = array_filter($recent_all);
+
+            echo '<ul>';
+            foreach( $recent_all as $recent ){
+                // push id to top_posts
+                array_push($top_posts, $recent["ID"]);
+                echo '<li><a href="' . get_permalink($recent["ID"]) . '">' .  $recent["post_type"] . ' ' . $recent["ID"] . ': ' . $recent["post_title"].'</a> </li> ';
+            }
+            echo '</ul>';
+            wp_reset_query();
+            print_r($top_posts);
+            echo '</div>';
+            echo '</div>';
+
+            // start html template
+            // $feedHTML .= '<div class="grid row">';
+            $feedHTML .= '<div class="grid row">';
+
+            // set grid item container
+
+            // grid
             $cols = get_sub_field('number_of_columns');
-            // echo 'number of columns: ' . $cols;
+            $col_grid_container;
+            switch ($cols) {
+                case
+                    $col_grid_container = '<div class="col-md-12 grid__item">';
+                    break;
+                case '2':
+                    $col_grid_container = '<div class="col-md-6 grid__item">';
+                    break;
+                case '3':
+                    $col_grid_container = '<div class="col-md-4 col-sm-6 grid__item">';
+                    break;
+                case '4':
+                    $col_grid_container = '<div class="col-md-3 col-sm-6 grid__item">';
+                    break;
+                default:
+                    $col_grid_container = '<div class="col-md-4 col-sm-6 grid__item">';
+            }
+
+            // main query
+            $paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
             $rpd_args = array( 
                 'post_type' => $post_type_array, 
-                'posts_per_page' => $number_of_posts_to_include, 
+                'posts_per_page' => $number_of_posts_to_include,
+                'post__not_in' => $top_posts,
                 'post_status' => 'publish', 
                 'order' => 'DESC', 
                 'orderby' => 'date', 
                 'paged' => $paged  
             );
-            
             $wp_query = new WP_Query( $rpd_args );
+
+            // loop
             while ($wp_query->have_posts() ) : $wp_query->the_post();
 
             $post_type = get_post_type();
-            switch ($cols) {
-                case '1':
-                    echo '<div class="col-md-12 grid__item">';
-                    break;
-                case '2':
-                    echo '<div class="col-md-6 grid__item">';
-                    break;
-                case '3':
-                    echo '<div class="col-md-4 col-sm-6 grid__item">';
-                    break;
-                case '4':
-                    echo '<div class="col-md-3 col-sm-6 grid__item">';
-                    break;
-                default:
-                    echo '<div class="col-md-4 col-sm-6 grid__item">';
-            }
 
+            echo $col_grid_container ;
             if($post_type == 'podcasts'):   
                 get_template_part( '/loop-templates/content', 'podcast' );
             elseif($post_type == 'newsletters'):
@@ -129,7 +216,7 @@ function build_feed(){
             
             echo '</div><!-- .row -->';
 
-            // SCROLLER
+            // scroller
             echo '<div class="row">';
             echo '<div class="col-lg-3"></div>';
             echo '<div class="col-lg-6">';
@@ -142,7 +229,7 @@ function build_feed(){
             echo '<div class="col-lg-3"></div>';
             echo '</div>';
 
-            // PAGINATION
+            // pagination
             echo '<div class="pagination">';
             next_posts_link( 'Older Entries', $wp_query->max_num_pages );
             previous_posts_link( 'Newer Entries' );
