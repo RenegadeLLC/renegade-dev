a2bdead25b59b081b737c76b6e951ede44374acb
Vince Golveo <vince.golveo@gmail.com>
2018-08-02 16:11:22 -0400

    update the single.php container and single post

--- wp-content/themes/understrap-child/post-templates/single-post.php
+++ wp-content/themes/understrap-child/post-templates/single-post.php
@@ -6,41 +6,62 @@
  */
 
 ?>
-<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+
+
+	<!-- content header -->
+	<div class="row">
+		<div class="content-section" style="background-color:#f2f2f2">
+			<h1><?php the_title(); ?></h1>
+		</div>
+	</div>
+	<div class="row">
+	<!-- left sdebar -->
+	<div class="col-md-2">
+	</div>
+	<div class="col-md-8">
+	<main class="site-main" id="main">
+<article <?php post_class();?> id="post-<?php the_ID();?>">
 
 	<header class="entry-header">
 
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		<?php the_title('<h1 class="entry-title">', '</h1>');?>
 
 		<div class="entry-meta">
 
-			<?php understrap_posted_on(); ?>
+			<?php understrap_posted_on();?>
 
 		</div><!-- .entry-meta -->
 
 	</header><!-- .entry-header -->
 
-	<?php echo get_the_post_thumbnail( $post->ID, 'large' ); ?>
+	<?php echo get_the_post_thumbnail($post->ID, 'large'); ?>
 
 	<div class="entry-content">
-
-		<?php the_content(); ?>
-
-		<?php understrap_post_nav(); ?>
-		
-		<?php
-		wp_link_pages( array(
-			'before' => '<div class="page-links">' . __( 'Pages:', 'understrap' ),
-			'after'  => '</div>',
-		) );
-		?>
-
-	</div><!-- .entry-content -->
-
-	<footer class="entry-footer">
-
-		<?php understrap_entry_footer(); ?>
-
-	</footer><!-- .entry-footer -->
+		<?php the_content();?>
 
 </article><!-- #post-## -->
+			</main><!-- #main -->
+
+		</div>
+
+		<!-- right sidebar -->
+		<div class="col-md-2">
+		</div>
+
+</div><!-- .row -->
+
+<div class="row">
+	<div class="col-md-12">
+		<div class="content-section">
+
+		<?php understrap_post_nav();?>
+
+		<?php
+wp_link_pages(array(
+    'before' => '<div class="page-links">' . __('Pages:', 'understrap'),
+    'after' => '</div>',
+));
+?>
+		</div>
+	</div>
+</div>
