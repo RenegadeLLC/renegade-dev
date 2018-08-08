480f2dd05e74c2455e68aa5f0d3f7fd2034c10b8
Vince Golveo <vince.golveo@gmail.com>
2018-08-08 16:59:26 -0400

    add first name to nav links in posts where it exists

--- wp-content/themes/understrap-child/functions.php
+++ wp-content/themes/understrap-child/functions.php
@@ -195,7 +195,9 @@ if ( ! function_exists ( 'understrap_post_nav' ) ) {
 	function understrap_post_nav() {
 		// Don't print empty markup if there's nowhere to navigate.
 		$previous = ( is_attachment() ) ? get_post( get_post()->post_parent ) : get_adjacent_post( false, '', true );
-		$next     = get_adjacent_post( false, '', false );
+        $next     = get_adjacent_post( false, '', false );
+        // exception to show first name on leadership pages
+        $rp_first_name = get_field('rp_first_name');
 
 		if ( ! $next && ! $previous ) {
 			return;
@@ -207,10 +209,10 @@ if ( ! function_exists ( 'understrap_post_nav' ) ) {
                 <?php
 
                     if ( get_previous_post_link() ) {
-                        previous_post_link( '<div class="nav-previous">%link</div>', _x( '<i class="fa fa-angle-left"></i>&nbsp;%title', 'Previous post link', 'understrap' ) );
+                        previous_post_link( '<div class="nav-previous">%link</div>', _x( '<i class="fa fa-angle-left"></i>&nbsp;'. $rp_first_name . '%title', 'Previous post link', 'understrap' ) );
                     }
                     if ( get_next_post_link() ) {
-                        next_post_link( '<div class="nav-next">%link</div>',     _x( '%title&nbsp;<i class="fa fa-angle-right"></i>', 'Next post link', 'understrap' ) );
+                        next_post_link( '<div class="nav-next">%link</div>',     _x( $rp_first_name . '&nbsp;%title&nbsp;<i class="fa fa-angle-right"></i>', 'Next post link', 'understrap' ) );
                     }
                 ?>
             </div><!-- .nav-links -->
