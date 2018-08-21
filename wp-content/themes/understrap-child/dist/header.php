15f85c4a4be7e27ce89e7d346be2afab16c83d9a
Vince Golveo <vince.golveo@gmail.com>
2018-08-21 14:42:20 -0400

    add meta referrer

--- wp-content/themes/understrap-child/dist/header.php
+++ wp-content/themes/understrap-child/dist/header.php
@@ -26,6 +26,7 @@ endif;
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="referrer" content="unsafe-url">
 	<meta name="mobile-web-app-capable" content="yes">
 	<meta name="apple-mobile-web-app-capable" content="yes">
 	<meta name="apple-mobile-web-app-title" content="<?php bloginfo( 'name' ); ?> - <?php bloginfo( 'description' ); ?>">
--- wp-content/themes/understrap-child/dist/loop-templates/content-newsletter.php
+++ wp-content/themes/understrap-child/dist/loop-templates/content-newsletter.php
@@ -12,11 +12,17 @@ $rn_link = get_permalink($post -> ID);
 $rn_introduction= get_field('rn_introduction' , $post -> ID);
 $rn_excerpt = limit_text($rn_introduction, 40);
 $rn_banner = get_field('rn_banner', $post -> ID);
+$rn_thumbnail_image = get_field('rn_thumbnail_image', $post -> ID);
 $rn_size = array('506', '169');
 
 $newsletter_html = '<div class="card">';
 $newsletter_html .= '<a href="' . $rn_link . '">';
-$newsletter_html .= wp_get_attachment_image($rn_banner, $rn_size );
+if($rn_thumbnail_image ):
+	$newsletter_html .= '<img src="' . $rn_thumbnail_image . '" alt="' . $rn_title  . '">';	
+elseif($rn_banner):
+	$newsletter_html .= wp_get_attachment_image($rn_banner, $rn_size );
+endif;
+
 $newsletter_html .= '<div class="card-body">';
 $newsletter_html .= '<h5 class="card-title">' . $rn_title . '</h5>';
 $newsletter_html .= '<p class="card-text">';
