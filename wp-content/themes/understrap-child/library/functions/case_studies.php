fefdb32d0d960ec8c60f3b9a856e67b928d65ba6
Anne Rothschild <annierothschild@gmail.com>
2018-08-31 12:32:48 -0400

    Update Podcast loop template & Case Studies loop
    use show image instead of profile for podcast excerpt display, and fix selected case studies display so alternate project link can be used instead of main single project link

--- wp-content/themes/understrap-child/library/functions/case_studies.php
+++ wp-content/themes/understrap-child/library/functions/case_studies.php
@@ -48,16 +48,24 @@ function build_case_studies(){
                 //start case studies repeater
                 if(have_rows('case_studies')):
                     while ( have_rows('case_studies') ) : the_row();
-                        
+
+                      
+
                         $case_study = get_sub_field('case_study');
                         $post = $case_study;
                         setup_postdata( $post );
 
                         $project_title = get_field('project_title', $post -> ID);
-                        $client_name = get_field('client_name');
+                       
+                        
                         $client = get_the_title($post -> ID);
-                     
+                        $alternate_project_url = get_field('alternate_project_url', $post -> ID);
                         $case_url = get_permalink($post -> ID);
+
+                        if($alternate_project_url != NULL):
+                            $case_url = $alternate_project_url;
+                        endif;
+                        
                         $service_type = get_field('service_type', $post -> ID);
                         $industry_vertical = get_field('industry_vertical', $post -> ID);
                         $project_thumbnail_image = get_field('project_thumbnail_image', $post -> ID);
@@ -65,15 +73,13 @@ function build_case_studies(){
                         $summary_text = get_field('summary_text', $post -> ID);
 
                         $caseHTML .= '<div class="col-lg-4 col-md-4 col-sm-12 case-ct">';
-                        // $caseHTML .= '<div>' . $client . '</div>';
-                        // $caseHTML .= '<div>' . $client_name . '</div>';
-                        if($client != 'Renegade' && $client !='Coming Soon'):
+                 
                         $caseHTML .= '<a href="' . $case_url . '">';
-                        endif;
+                     
                         $caseHTML .= '<div class="case-thumb-ct"><img src="'. $project_thumbnail_image . '" title="' . $client . ': ' . $project_title . '"></div><!-- .case-image-ct -->';
-                        if($client != 'Renegade' && $client !='Coming Soon'):
+                        if($client != 'Renegade' && $client != 'All Clients'):
                             $caseHTML .= '<div class="case-info"><h4>' . $client . ' <span class="project-title">' . $project_title . '</span></h4></div><!-- .case-info -->';
-                        endif;
+                       endif;
                         $caseHTML .= '</a></div><!-- .case-ct-->';
     
                         wp_reset_postdata();
